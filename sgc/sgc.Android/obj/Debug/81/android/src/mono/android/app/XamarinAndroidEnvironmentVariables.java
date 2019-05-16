008f1b7aa0266cbbfc4bfdf261d62e0badcd1e1a
root <root@DESKTOP-ABDDVL2.localdomain>
2019-05-16 15:55:46 -0400

    inicio de proyecto

--- sgc/sgc.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ sgc/sgc.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "04961d92-2db6-4f8f-af3c-8eb2751a2cca",
+		"XAMARIN_BUILD_ID", "76f55d0e-ea2b-4215-8139-dbb7e15d30ec",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
